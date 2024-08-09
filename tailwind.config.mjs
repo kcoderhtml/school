2497bde8494ee160accd03411ac0b0c4b26bd85c
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-09 15:18:13 -0400

    feat: add @tailwindcss/typography plugin for markdown styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("@catppuccin/tailwindcss")],
+  plugins: [
+    require("@catppuccin/tailwindcss"),
+    require("@tailwindcss/typography"),
+  ],
 };
